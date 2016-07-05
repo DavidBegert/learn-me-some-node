3e2fb2adfa1b4007b0c07ae50a40871254bd80f4
David Begert <dbegert@usc.edu>
2016-07-05 12:09:00 -0700

    exercise12 (http_uppercase) passed

---
+++ http_uppercase.js
@@ -0,0 +1,12 @@
+var map = require('through2-map')  
+const http = require('http')
+
+
+var server = http.createServer(function(req,res) {
+  req.pipe(map(function (chunk) {  
+    return chunk.toString().toUpperCase(); 
+  })).pipe(res) 
+
+});
+
+server.listen(process.argv[2]);
